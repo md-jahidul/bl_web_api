fe85c5f284086359eb229454364ef619bbdeb901
emdad.ullah <emdad.ullah@bs-23.com>
2019-09-18 12:11:15 +0600

    Header footer settings done

--- app/Http/Controllers/API/V1/HeaderFooterMenuApiController.php
+++ app/Http/Controllers/API/V1/HeaderFooterMenuApiController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use App\Models\FooterMenu;
 use App\Models\Menu;
+use App\Models\Config;
 use Illuminate\Database\QueryException;
 
 class HeaderFooterMenuApiController extends Controller
@@ -24,7 +25,29 @@ class HeaderFooterMenuApiController extends Controller
                                     ->where('status', 1)
                                     ->orderBy('display_order')
                                     ->get();
-                                    
+
+
+            $h_settings = Config::where('key','site_logo')
+                        ->orWhere('key','logo_alt_text')
+                        ->get();
+
+            $header_settings = [];                        
+            foreach ($h_settings as $settings) {
+                $header_settings[ $settings->key ] =  $settings->value;
+            }
+
+           
+
+            $f_settings = Config::whereNotIn('key',['site_logo','logo_alt_text'])
+                                      ->get();
+
+            $footer_settings = [];                        
+            foreach ($f_settings as $settings) {
+                $footer_settings[$settings->key] = $settings->value;
+            }
+
+            
+
             if (isset($footerMenu)) {
                 return response()->json(
                     [
@@ -33,10 +56,12 @@ class HeaderFooterMenuApiController extends Controller
                         'message' => 'Data Found!',
                         'data' => [
                             'header' => [
-                                'menu' => $headerMenu
+                                'menu' => $headerMenu,
+                                'settings' => $header_settings
                             ],
                             'footer' => [
-                               'menu' => $footerMenu
+                               'menu' => $footerMenu,
+                               'settings' => $footer_settings
                             ]
                         ]
                     ]
