fb5e3a1384a9dbf7d516c3d2fa3406b4d3877ef1
emdad.ullah <emdad.ullah@bs-23.com>
2019-10-03 12:10:38 +0600

    lifestyle offer filter

--- app/Http/Controllers/API/V1/HomeDataDynamicApiController.php
+++ app/Http/Controllers/API/V1/HomeDataDynamicApiController.php
@@ -94,12 +94,12 @@ class HomeDataDynamicApiController extends Controller
                                           ])->get();
 
         $slider->data = DB::table('partner_offers as po')
+                        ->where('po.show_in_home',1)
+                        ->where('po.is_active',1)
                         ->join('partners as p', 'po.partner_id', '=', 'p.id')
                         ->join('partner_categories as pc', 'p.partner_category_id', '=', 'pc.id') // you may add more joins
                         ->select('po.*', 'pc.name_en AS offer_type_en', 'pc.name_bn AS offer_type_bn', 'p.company_name_en','p.company_name_bn','p.company_logo')
                         ->get();
-
-
 
         return $slider;
     }
