40ee962dbc9d4e26fdfc4d78ec1e5cc48c1ab0cc
emdad.ullah <emdad.ullah@bs-23.com>
2019-11-03 16:14:35 +0600

    Remove unnesessary code

--- app/Http/Controllers/API/V1/HeaderFooterMenuApiController.php
+++ app/Http/Controllers/API/V1/HeaderFooterMenuApiController.php
@@ -60,47 +60,4 @@ class HeaderFooterMenuApiController extends Controller
             return response()->error('Data Not Found!', $e->getMessage());  
         }
     }
-
-    public function getConfig()
-    {
-        try{
-            $headerMenu = Menu::with('children')
-                ->where('parent_id', 0)
-                ->where('status', 1)
-                ->get();
-
-            if (isset($footerMenu)) {
-                return response()->json(
-                    [
-                        'status' => 200,
-                        'success' => true,
-                        'message' => 'Data Found!',
-                        'data' => [
-                            'header' => [
-                                'menu' => $headerMenu
-                            ],
-                            'footer' => [
-                                'menu' => $footerMenu
-                            ]
-                        ]
-                    ]
-                );
-            }
-            return response()->json(
-                [
-                    'status' => 400,
-                    'success' => false,
-                    'message' => 'Data Not Found!'
-                ]
-            );
-        }catch (QueryException $e) {
-            return response()->json(
-                [
-                    'status' => 403,
-                    'success' => false,
-                    'error-message' => explode('|', $e->getMessage())[0],
-                ]
-            );
-        }
-    }
 }
