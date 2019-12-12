59e6840cb5b0988f74df218b8e4adb7de63d3031
jahidul <jahidul@bs-23.net>
2019-12-12 18:11:44 +0600

    priyojon url bug fixed

--- app/Http/Resources/AboutPriyojonResource.php
+++ app/Http/Resources/AboutPriyojonResource.php
@@ -18,11 +18,9 @@ class AboutPriyojonResource extends JsonResource
             "id" =>  $this->id ?? null,
             "details_en" => $this->details_en ?? null,
             "details_bn" => $this->details_bn ?? null,
-            "left_side_img" => ($this->left_side_img != '') ? env("IMAGE_HOST_URL") . "/" . $this->left_side_img : null,
-            "right_side_ing" =>($this->right_side_ing != '') ? env("IMAGE_HOST_URL") . "/" . $this->right_side_ing : null,
+            "left_side_img" => ($this->left_side_img != '') ? env("IMAGE_HOST_URL") . $this->left_side_img : null,
+            "right_side_ing" =>($this->right_side_ing != '') ? env("IMAGE_HOST_URL") . $this->right_side_ing : null,
             "other_attributes" => $this->other_attributes,
-            // created_at: "2019-11-26 13:35:38", Optional
-            // updated_at: "2019-11-26 13:35:38", Optional
         ];
     }
 }
