c606aab88e7174b7d10403fc3ffdb1c4862fbab2
rchowdhury-bs23 <r.chowdhury@brainstation-23.com>
2020-01-08 15:16:19 +0600

    recharge offer

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -62,17 +62,28 @@ class ProductRepository extends BaseRepository
         //TODO:add filter by start and end date
         //
         # check price range
-        $check_range = ProductPriceSlab::where('range_start', '<=', (int)$amount)->where('range_end', '>=', (int)$amount)->first()->id;        
-    
+        $check_product_code = ProductPriceSlab::where('range_start', '<=', (int)$amount)->where('range_end', '>=', (int)$amount)->first()->product_code;
+
+        $check_product_code = !empty($check_product_code) ? $check_product_code : null;
         
         return $this->model->join('product_cores', 'products.product_code', 'product_cores.product_code')
             ->selectRaw('products.*, product_cores.activation_ussd as ussd_en, product_cores.balance_check_ussd, product_cores.mrp_price as price_tk,
              product_cores.validity as validity_days,product_cores.validity_unit, product_cores.internet_volume_mb,product_cores.sms_volume,product_cores.minute_volume,product_cores.call_rate,product_cores.sms_rate')
             ->whereIn('products.purchase_option', ['recharge'])
             ->where('products.status', 1)
+            // ->where('products.sim_category_id', 1) // Check prepaid sim type
+            ->whereHas('sim_category', function ($query) {
+                $query->where('alias', 'prepaid');
+            })
             // ->where('product_cores.mrp_price', '=', $amount)
-            ->where(function($query) use ($amount, $check_range){
-                return $query->where('product_cores.mrp_price', '=', $amount)->orWhere('products.price_slabs_id', '=', $check_range);
+            ->where(function($query) use ($amount, $check_product_code){
+                if( !empty($check_product_code) ){
+                    return $query->where('product_cores.mrp_price', '=', $amount)->orWhere('product_cores.recharge_product_code', '=', $check_product_code);
+                }
+                else{
+                    return $query->where('product_cores.mrp_price', '=', $amount);
+                }
+                
 
             })
             ->orderBy('product_cores.mrp_price')
