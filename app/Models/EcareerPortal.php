bed40956e4522880dea2f4ef3539949f0f5fd57d
Raihan Chowdhury <r.chowdhury@brainstation-23.com>
2020-03-09 11:41:06 +0600

    ecareer table name changes to model

--- app/Models/EcareerPortal.php
+++ app/Models/EcareerPortal.php
@@ -12,7 +12,7 @@ class EcareerPortal extends Model
      *
      * @var string
      */
-    protected $table = 'ecarrer_portals';
+    protected $table = 'ecareer_portals';
 
    protected $fillable = ['title_en', 'title_bn', 'slug', 'description_en', 'description_bn', 'image', 'video', 'alt_text', 'category', 'route_slug', 'category_type', 'additional_info', 'is_active', 'has_items', 'deleted_at'];
 
