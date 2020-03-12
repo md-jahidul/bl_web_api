25d50851c148957eab0a0a87876715a54841f5c5
Raihan Chowdhury <r.chowdhury@brainstation-23.com>
2020-03-12 16:27:20 +0600

    app and service api fixing

--- app/Services/AppServiceDetailsService.php
+++ app/Services/AppServiceDetailsService.php
@@ -15,175 +15,180 @@ use App\Repositories\AppServiceProductDetailsRepository;
 
 class AppServiceDetailsService
 {
-    use CrudTrait;
-    use FileTrait;
+	use CrudTrait;
+	use FileTrait;
 
 
-    /**
-     * @var $appServiceProductDetailsRepository
-     */
-    protected $appServiceProductDetailsRepository;
-    
+	/**
+	 * @var $appServiceProductDetailsRepository
+	 */
+	protected $appServiceProductDetailsRepository;
+	
 
-    /**
-     * @var AppServiceProductDetailsRepository
-     */
-    protected $appServiceProductRepository;
-
-
-    /**
-     * AppServiceProductService constructor.
-     * @param AppServiceProductDetailsRepository $appServiceProductDetailsRepository
-     */
-    public function __construct(AppServiceProductDetailsRepository $appServiceProductDetailsRepository, AppServiceProductRepository $appServiceProductRepository)
-    {
-        $this->appServiceProductDetailsRepository = $appServiceProductDetailsRepository;
-        $this->appServiceProductRepository = $appServiceProductRepository;
-        $this->setActionRepository($appServiceProductDetailsRepository);
-    }
+	/**
+	 * @var AppServiceProductDetailsRepository
+	 */
+	protected $appServiceProductRepository;
 
 
-    /**
-     * [getProductInformationByID description]
-     * @param  [type] $product_id [description]
-     * @return [type]             [description]
-     */
-    public function getProductInformationByID($product_id)
-    {
-        return $this->appServiceProductRepository->findOne($product_id, 'appServiceTab' );
-    }
-
-    /**
-     * [getProductDetailsBanner description]
-     * @param  [type] $product_id [description]
-     * @return [type]             [description]
-     */
-    public function getProductDetailsOthersInfo($product_id)
-    {
-        $results = null;
-
-        $get_product_details_banner = $this->appServiceProductDetailsRepository->appServiceDetailsOtherInfo($product_id);
-
-        if( !empty($get_product_details_banner) ){
-            $results['banner']['image'] = !empty($get_product_details_banner->image) ? config('filesystems.image_host_url') . $get_product_details_banner->image : null;
-            $results['banner']['alt_text'] = $get_product_details_banner->alt_text;
-            
-            $all_releated_products_ids = $get_product_details_banner->other_attributes;
-            $all_releated_products_ids = isset($all_releated_products_ids['related_product_id']) ? $all_releated_products_ids['related_product_id'] : null;
-            $get_all_releated_products = $this->appServiceProductRepository->findByIds($all_releated_products_ids);
-            $results['releated_products']['title_en'] = $get_product_details_banner->title_en;
-            $results['releated_products']['title_bn'] = $get_product_details_banner->title_bn;
-            $results['releated_products']['products'] = !empty($get_all_releated_products) ? $get_all_releated_products : null;
-        }
-
-        return $results;
-
-    }
+	/**
+	 * AppServiceProductService constructor.
+	 * @param AppServiceProductDetailsRepository $appServiceProductDetailsRepository
+	 */
+	public function __construct(AppServiceProductDetailsRepository $appServiceProductDetailsRepository, AppServiceProductRepository $appServiceProductRepository)
+	{
+		$this->appServiceProductDetailsRepository = $appServiceProductDetailsRepository;
+		$this->appServiceProductRepository = $appServiceProductRepository;
+		$this->setActionRepository($appServiceProductDetailsRepository);
+	}
 
 
-    /**
-     * [getDetailsSectionComponents description]
-     * @param  [type] $product_id [description]
-     * @return [type]             [description]
-     */
-    public function getDetailsSectionComponents($product_id, $component_type = [])
-    {
+	/**
+	 * [getProductInformationByID description]
+	 * @param  [type] $product_id [description]
+	 * @return [type]             [description]
+	 */
+	public function getProductInformationByID($product_id)
+	{
+		return $this->appServiceProductRepository->findOne($product_id, 'appServiceTab' );
+	}
 
-        $data = null;
+	/**
+	 * [getProductDetailsBanner description]
+	 * @param  [type] $product_id [description]
+	 * @return [type]             [description]
+	 */
+	public function getProductDetailsOthersInfo($product_id)
+	{
+		$results = null;
 
-        $results = $this->appServiceProductDetailsRepository->getSectionsComponents($product_id);
+		$get_product_details_banner = $this->appServiceProductDetailsRepository->appServiceDetailsOtherInfo($product_id);
 
-        if( !empty($results) && count($results) > 0 ){
+		if( !empty($get_product_details_banner) ){
+			$results['banner']['image'] = !empty($get_product_details_banner->image) ? config('filesystems.image_host_url') . $get_product_details_banner->image : null;
+			$results['banner']['alt_text'] = $get_product_details_banner->alt_text;
+			
+			$all_releated_products_ids = $get_product_details_banner->other_attributes;
+			$all_releated_products_ids = isset($all_releated_products_ids['related_product_id']) ? $all_releated_products_ids['related_product_id'] : null;
+			$get_all_releated_products = $this->appServiceProductRepository->findByIds($all_releated_products_ids);
+			$results['releated_products']['title_en'] = $get_product_details_banner->title_en;
+			$results['releated_products']['title_bn'] = $get_product_details_banner->title_bn;
+			$results['releated_products']['products'] = !empty($get_all_releated_products) ? $get_all_releated_products : null;
+		}
 
-            foreach ($results as $value) {
-                
-                $sub_data = [];
+		return $results;
 
-                $parent_data['title_en'] = $value->title_en;
-                $parent_data['title_bn'] = $value->title_bn;
-                $parent_data['slug'] = $value->slug;
-
-                $sub_data['section_header'] = $parent_data;
-
-                
-
-                if( !empty($value->detailsComponent) && count($value->detailsComponent) > 0 ){
-
-                    
-
-                    foreach ($value->detailsComponent as $item) {
-                        
-                        $sub_item = [];
-
-                        $sub_item['title_en'] = $item->title_en;
-                        $sub_item['title_bn'] = $item->title_bn;
-                        $sub_item['slug'] = $item->slug;
-                        $sub_item['component_type'] = $item->component_type;
-                        $sub_item['description_en'] = $item->description_en;
-                        $sub_item['description_bn'] = $item->description_bn;
-                        $sub_item['editor_en'] = $item->editor_en;
-                        $sub_item['editor_bn'] = $item->editor_bn;
-                        $sub_item['image'] = !empty($item->image) ? config('filesystems.image_host_url') . $item->image : null;
-                        $sub_item['alt_text'] = $item->alt_text;
-                        $sub_item['video'] = !empty($item->video) ? config('filesystems.image_host_url') . $item->video : null;
-                        $sub_item['alt_links'] = $item->alt_links;
-
-                        
-                        if( $item->multiple_attributes != null && !empty($item->multiple_attributes) ){
-                            $res = json_decode($item->multiple_attributes, true);
-                            
-                            if( !empty($res) && count($res) > 0 ){
-
-                                $multi_res = array_map(function($value){
-
-                                    if( isset($value['image_url']) ){
-                                        $value['image_url'] = config('filesystems.image_host_url') . $value['image_url'];
-                                    }
-
-                                    // if( $value['status'] == 0 ){
-                                    // }
-                                    
-                                    return $value;
-
-                                }, $res);
-
-                                $sub_item['multiple_attributes'] = array_values($multi_res);
-                            }
-                            else{
-                                $sub_item['multiple_attributes'] = null;
-                            }
+	}
 
 
-                        }
-                        else{
-                            $sub_item['multiple_attributes'] = null;
-                        }
-                        
-                        $sub_item['other_attributes'] = $item->other_attributes;
+	/**
+	 * [getDetailsSectionComponents description]
+	 * @param  [type] $product_id [description]
+	 * @return [type]             [description]
+	 */
+	public function getDetailsSectionComponents($product_id, $component_type = [])
+	{
 
-                        if( !empty($item->other_attributes) && count($item->other_attributes) >0 ){
-                            foreach ($item->other_attributes as $key => $value) {
-                                $sub_item[$key] = $value;
-                            }
-                        }
+		$data = null;
 
-                        $sub_data['component'][] = $sub_item;
-
-                    }
-                }
-                else{
-                    $sub_data['component'] = null;
-                }
-
-                $data[] = $sub_data;
-
-            }
-        }
-        
+		$results = $this->appServiceProductDetailsRepository->getSectionsComponents($product_id);
 
 
-        return $data;
-    }
 
-    
+		if( !empty($results) && count($results) > 0 ){
+
+			foreach ($results as $value) {
+				
+				$sub_data = [];
+
+				$parent_data['title_en'] = $value->title_en;
+				$parent_data['title_bn'] = $value->title_bn;
+				$parent_data['slug'] = $value->slug;
+
+				$sub_data['section_header'] = $parent_data;
+
+				if( !empty($value->detailsComponent) && count($value->detailsComponent) > 0 ){
+
+
+					foreach ($value->detailsComponent as $item) {
+						
+						$sub_item = [];
+
+						$sub_item['title_en'] = $item->title_en;
+						$sub_item['title_bn'] = $item->title_bn;
+						$sub_item['slug'] = $item->slug;
+						$sub_item['component_type'] = $item->component_type;
+						$sub_item['description_en'] = $item->description_en;
+						$sub_item['description_bn'] = $item->description_bn;
+						$sub_item['editor_en'] = $item->editor_en;
+						$sub_item['editor_bn'] = $item->editor_bn;
+						$sub_item['image'] = !empty($item->image) ? config('filesystems.image_host_url') . $item->image : null;
+						$sub_item['alt_text'] = $item->alt_text;
+						$sub_item['video'] = !empty($item->video) ? config('filesystems.image_host_url') . $item->video : null;
+						$sub_item['alt_links'] = $item->alt_links;
+
+						// Multiple attributed formated
+						if( $item->multiple_attributes != null && !empty($item->multiple_attributes) ){
+							$res = json_decode($item->multiple_attributes, true);
+							
+							if( !empty($res) && count($res) > 0 ){
+
+								$multi_res = array_map(function($value){
+
+									if( isset($value['image_url']) ){
+										$value['image_url'] = config('filesystems.image_host_url') . $value['image_url'];
+									}
+
+									if( $value['status'] == 0 ){
+										return null;
+									}
+									
+									return $value;
+
+								}, $res);
+
+								$final_multi_res = array_values(array_filter($multi_res));
+								usort($final_multi_res, function($a, $b){return strcmp($a["display_order"], $b["display_order"]);});
+
+								$sub_item['multiple_attributes'] = $final_multi_res;
+							}
+							else{
+								$sub_item['multiple_attributes'] = null;
+							}
+
+							
+
+						}
+						else{
+							$sub_item['multiple_attributes'] = null;
+						}
+
+						$sub_item['other_attributes'] = $item->other_attributes;
+
+						if( !empty($item->other_attributes) ){
+							$other_attr_array = json_decode($item->other_attributes, true);
+							foreach ($other_attr_array as $key => $value) {
+								$sub_item[$key] = $value;
+							}
+						}
+
+						$sub_data['component'][] = $sub_item;
+
+					}
+				}
+				else{
+					$sub_data['component'] = null;
+				}
+
+				$data[] = $sub_data;
+
+			}
+		}
+		
+		
+
+		return $data;
+	}
+
+	
 }
