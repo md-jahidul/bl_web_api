ae87663341d6b2504f06f20336749730ae95d0c3
unknown <mahmudbd1000@gmail.com>
2020-03-24 20:35:51 +0600

    for update from dev

--- app/Http/Controllers/API/V1/RoamingController.php
+++ app/Http/Controllers/API/V1/RoamingController.php
@@ -36,86 +36,15 @@ class RoamingController extends Controller
     }
 
     /**
-     * Get package category page data
+     * Get Country List
      * 
      * @param No
      * @return Json Response
-     * @Bulbul Mahmud Nito || 24/02/2020
+     * @Bulbul Mahmud Nito || 23/03/2020
      */
-    public function packages()
+    public function getCountries()
     {
-        return $this->packageService->getPackages();
-    }
-
-    /**
-     * Get package details
-     * 
-     * @param No
-     * @return Json Response
-     * @Bulbul Mahmud Nito || 24/02/2020
-     */
-    public function packageById($packageId)
-    {
-        return $this->packageService->getPackageById($packageId);
-    }
-
-    /**
-     * Get Internet package
-     * 
-     * @param No
-     * @return Json Response
-     * @Bulbul Mahmud Nito || 24/02/2020
-     */
-    public function internet()
-    {
-        return $this->internetService->getInternetPackage();
-    }
-    
-    /**
-     * Get Internet package details
-     * 
-     * @param $internetId
-     * @return Json Response
-     * @Bulbul Mahmud Nito || 15/03/2020
-     */
-    public function internetDetails($internetId)
-    {
-        return $this->internetService->getInternetDetails($internetId);
-    }
-    /**
-     * Give like and get total likes
-     * 
-     * @param No
-     * @return Json Response
-     * @Bulbul Mahmud Nito || 15/03/2020
-     */
-    public function internetLike($internetId)
-    {
-        return $this->internetService->saveInternetLike($internetId);
-    }
-
-    /**
-     * Get Enterprise Solution
-     * 
-     * @param $type (business-solusion,iot,others)
-     * @return Json Response
-     * @Bulbul Mahmud Nito || 24/02/2020
-     */
-    public function enterpriseSolusion($type)
-    {
-        return $this->enterpriseService->getOtherService($type);
-    }
-    
-    /**
-     * Get Enterprise Solution
-     * 
-     * @param $serviceId
-     * @return Json Response
-     * @Bulbul Mahmud Nito || 24/02/2020
-     */
-    public function enterpriseProductDetails($serviceId)
-    {
-        return $this->enterpriseService->getServiceById($serviceId);
+        return $this->roammingService->getCategories();
     }
 
 }
