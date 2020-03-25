973f46c9ffe3576c8f26b787b36278fac5251bb4
BS289 <nahidul.hasan@bs-23.net>
2020-03-25 09:14:26 +0600

    add link attribute

--- app/Http/Resources/AboutUsEcareerItemResource.php
+++ app/Http/Resources/AboutUsEcareerItemResource.php
@@ -19,8 +19,9 @@ class AboutUsEcareerItemResource extends JsonResource
             'title_en'            => $this->title_en ?? null,
             'title_bn'            => $this->title_bn ?? null,
             'description_en'      => $this->description_en ?? null,
-            'description_bn'      => $this->title_en ?? null,
+            'description_bn'      => $this->description_bn ?? null,
             'is_active'           => $this->is_active ?? null,
+            'link'           => $this->alt_links ?? null,
             'image'               => env('IMAGE_HOST_URL') . "/" . $this->image ?? null,
 
         ];
