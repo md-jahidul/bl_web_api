c17c11581200f3f4a99fabc3cb8c957d50f24e67
unknown <mahmudbd1000@gmail.com>
2020-04-26 14:25:34 +0600

    full text search modification

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -81,7 +81,6 @@ class SearchService extends BaseService {
 
     public function searchData($keyword) {
         $keywords = $this->searchRepository->searchData($keyword);
-      return $keywords;
 
         $data = [];
 
