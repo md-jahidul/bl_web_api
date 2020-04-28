9a88dbcdf725755fc7dc2fe4f14fd581533ed386
unknown <mahmudbd1000@gmail.com>
2020-04-28 14:32:24 +0600

    priyojon details

--- app/Http/Resources/PartnerOfferResource.php
+++ app/Http/Resources/PartnerOfferResource.php
@@ -4,29 +4,42 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PartnerOfferResource extends JsonResource
-{
+class PartnerOfferResource extends JsonResource {
+
     /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    public function toArray($request)
-    {
+    public function toArray($request) {
+        if (!empty($this->phone)) {
+            $phone = json_decode($this->phone);
+        }
+        if (!empty($this->location)) {
+            $location = json_decode($this->location);
+        }
+
         return [
-            "id" =>  $this->id ?? null,
+            "id" => $this->id ?? null,
             "partner_id" => $this->partner_id ?? null,
             "product_code" => $this->product_code ?? null,
+            "area_en" => $this->area_en ?? null,
+            "area_bn" => $this->area_bn ?? null,
+            "phone_en" => $phone->en ?? null,
+            "phone_bn" => $phone->bn ?? null,
+            "location_en" => $location->en ?? null,
+            "location_bn" => $location->bn ?? null,
+            "map_link" => $this->map_link ?? null,
             "validity_en" => $this->validity_en ?? null,
             "validity_bn" => $this->validity_bn ?? null,
             "offer_unit" => $this->offer_unit ?? null,
             "offer_value" => $this->offer_value ?? null,
             "offer_scale" => $this->offer_scale ?? null,
             "get_offer_msg_en" => $this->get_offer_msg_en ?? null,
-            "get_offer_msg_bn" => $this->get_offer_msg_bn ?? null  ,
-            "btn_text_en"  => $this->btn_text_en ?? null ,
-            "btn_text_bn" => $this->btn_text_bn ?? null ,
+            "get_offer_msg_bn" => $this->get_offer_msg_bn ?? null,
+            "btn_text_en" => $this->btn_text_en ?? null,
+            "btn_text_bn" => $this->btn_text_bn ?? null,
             "campaign_img" => (!empty($this->campaign_img)) ? config("filesystems.image_host_url") . $this->campaign_img : null,
             "is_campaign" => $this->is_campaign ?? null,
             "show_in_home" => $this->show_in_home ?? null,
@@ -37,8 +50,9 @@ class PartnerOfferResource extends JsonResource
             "offer_type_en" => $this->offer_type_en ?? null,
             "offer_type_bn" => $this->offer_type_bn ?? null,
             "company_name_en" => $this->company_name_en ?? null,
-            "company_name_bn" =>  $this->company_name_bn,
+            "company_name_bn" => $this->company_name_bn,
             "company_logo" => (!empty($this->company_logo)) ? config("filesystems.image_host_url") . $this->company_logo : null
         ];
     }
+
 }
--- app/Repositories/PartnerOfferRepository.php
+++ app/Repositories/PartnerOfferRepository.php
@@ -33,9 +33,11 @@ class PartnerOfferRepository extends BaseRepository
     {
         $priyojonOffers = DB::table('partner_offers as po')
             ->where('po.is_active',1)
+            ->where('po.show_in_home',1)
             ->join('partners as p', 'po.partner_id', '=', 'p.id')
+            ->join('partner_area_list as a', 'po.area_id', '=', 'a.id')
             ->join('partner_categories as pc', 'p.partner_category_id', '=', 'pc.id') // you may add more joins
-            ->select('po.*', 'pc.name_en AS offer_type_en', 'pc.name_bn AS offer_type_bn', 'p.company_name_en','p.company_name_bn','p.company_logo')
+            ->select('po.*', 'a.area_en', 'a.area_bn', 'pc.name_en AS offer_type_en', 'pc.name_bn AS offer_type_bn', 'p.company_name_en','p.company_name_bn','p.company_logo')
             ->orderBy('po.display_order')
             ->get();
 
