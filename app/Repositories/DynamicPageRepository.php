8726bc357fe75ba29b105beb3b25e9ddff18a964
jahidul <jahidul@bs-23.net>
2020-12-28 20:02:39 +0600

    dynamic page issue fixed

--- app/Repositories/DynamicPageRepository.php
+++ app/Repositories/DynamicPageRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Models\FrontEndDynamicRoute;
 use App\Models\OtherDynamicPage;
 
 class DynamicPageRepository extends BaseRepository {
@@ -10,26 +11,30 @@ class DynamicPageRepository extends BaseRepository {
 
     public function page($slug)
     {
-        return $this->model->where('url_slug', $slug)
-            ->select(
-                'id', 'page_header', 'schema_markup',
-                'banner_image_url', 'banner_mobile_view',
-                'alt_text', 'page_name_en', 'page_name_bn',
-                'page_content_en', 'page_content_bn',
-                'url_slug'
-            )
-            ->with(['components' => function($q){
-                $q->orderBy('component_order', 'ASC')
-                    ->where('page_type', 'other_dynamic_page')
-                    ->select(
-                        'id', 'section_details_id', 'page_type',
-                        'component_type', 'title_en', 'title_bn',
-                        'editor_en', 'editor_bn', 'extra_title_bn',
-                        'extra_title_en', 'multiple_attributes',
-                        'video', 'image', 'alt_text', 'other_attributes'
-                    )
-                    ->where('status', 1);
-            }])
-            ->first();
+        $dynamicPageSlug = FrontEndDynamicRoute::where('url', $slug)->first();
+
+        if (isset($dynamicPageSlug->key)) {
+            return $this->model->where('url_slug', $dynamicPageSlug->key)
+                ->select(
+                    'id', 'page_header', 'schema_markup',
+                    'banner_image_url', 'banner_mobile_view',
+                    'alt_text', 'page_name_en', 'page_name_bn',
+                    'page_content_en', 'page_content_bn',
+                    'url_slug'
+                )
+                ->with(['components' => function($q){
+                    $q->orderBy('component_order', 'ASC')
+                        ->where('page_type', 'other_dynamic_page')
+                        ->select(
+                            'id', 'section_details_id', 'page_type',
+                            'component_type', 'title_en', 'title_bn',
+                            'editor_en', 'editor_bn', 'extra_title_bn',
+                            'extra_title_en', 'multiple_attributes',
+                            'video', 'image', 'alt_text', 'other_attributes'
+                        )
+                        ->where('status', 1);
+                }])
+                ->first();
+        }
     }
 }
