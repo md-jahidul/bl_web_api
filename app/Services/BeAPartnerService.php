3009adbbb80496cec9d349a1342204cfb456b421
jahidul <jahidul@bs-23.net>
2020-12-31 16:28:07 +0600

    add header bangla in ethics-and-compliance

--- app/Services/BeAPartnerService.php
+++ app/Services/BeAPartnerService.php
@@ -54,20 +54,29 @@ class BeAPartnerService extends ApiBaseService
     {
         $beAPartnerData = $this->beAPartnerRepository->getOneData();
         $components = $this->componentRepository->getComponentByPageType(self::PAGE_TYPE);
-        $data = [
-          'title_en' => $beAPartnerData->title_en,
-          'title_bn' => $beAPartnerData->title_bn,
-          'description_en' => $beAPartnerData->description_en,
-          'description_bn' => $beAPartnerData->description_bn,
-          'vendor_button_en' => $beAPartnerData->vendor_button_en,
-          'vendor_button_bn' => $beAPartnerData->vendor_button_bn,
-          'vendor_portal_url' => $beAPartnerData->vendor_portal_url,
-          'interested_button_en' => $beAPartnerData->interested_button_en,
-          'interested_button_bn' => $beAPartnerData->interested_button_bn,
-          'interested_url' => $beAPartnerData->interested_url,
-          'banner_image' => $beAPartnerData->banner_image,
-          'components' => $components
-        ];
+
+        $data = [];
+        if (isset($beAPartnerData)) {
+            $data = [
+                'title_en' => $beAPartnerData->title_en,
+                'title_bn' => $beAPartnerData->title_bn,
+                'description_en' => $beAPartnerData->description_en,
+                'description_bn' => $beAPartnerData->description_bn,
+                'vendor_button_en' => $beAPartnerData->vendor_button_en,
+                'vendor_button_bn' => $beAPartnerData->vendor_button_bn,
+                'vendor_portal_url' => $beAPartnerData->vendor_portal_url,
+                'interested_button_en' => $beAPartnerData->interested_button_en,
+                'interested_button_bn' => $beAPartnerData->interested_button_bn,
+                'interested_url' => $beAPartnerData->interested_url,
+                'page_header' => $beAPartnerData->page_header,
+                'page_header_bn' => $beAPartnerData->page_header_bn,
+                'schema_markup' => $beAPartnerData->schema_markup,
+                'url_slug_en' => $beAPartnerData->url_slug_en,
+                'url_slug_bn' => $beAPartnerData->url_slug_bn,
+                'banner_image' => $beAPartnerData->banner_image,
+                'components' => $components,
+            ];
+        }
         return $this->sendSuccessResponse($data, 'Be a partner Data');
     }
 
