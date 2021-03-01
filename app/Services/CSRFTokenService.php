f3d1bc6ddd612785bcdea41fbbf899a7ba8aed05
jahidul <jahidul@bs-23.net>
2021-03-01 12:03:39 +0600

    CSRF token R&D

--- app/Services/CSRFTokenService.php
+++ app/Services/CSRFTokenService.php
@@ -2,21 +2,6 @@
 
 namespace App\Services;
 
-use App\Enums\HttpStatusCode;
-use App\Http\Resources\AboutUsEcareerResource;
-use App\Http\Resources\AboutUsResource;
-use App\Http\Resources\ManagementResource;
-use App\Http\Resources\SliderImageResource;
-use App\Repositories\AboutUsEcareerRepository;
-use App\Repositories\AboutUsRepository;
-use App\Repositories\EcareerPortalRepository;
-use App\Repositories\ManagementRepository;
-use App\Repositories\SliderImageRepository;
-use App\Repositories\SliderRepository;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Session;
 
 class CSRFTokenService extends ApiBaseService
 {
