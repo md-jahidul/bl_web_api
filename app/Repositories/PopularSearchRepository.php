f39ccfb5c7a35598296d174c714c18597fb00e3b
jahidul <jahidul@bs-23.net>
2021-05-23 18:08:25 +0600

    add bangla keyword and url in website search

--- app/Repositories/PopularSearchRepository.php
+++ app/Repositories/PopularSearchRepository.php
@@ -14,7 +14,7 @@ class PopularSearchRepository extends BaseRepository {
     public $modelName = SearchPopularKeywords::class;
 
     public function getResults($limit) {
-         $response = $this->model->select('keyword', 'url as product_url')
+         $response = $this->model->select('keyword', 'keyword_bn', 'url as product_url', 'url_bn as product_url_bn')
                  ->offset(0)->limit($limit)
                  ->where('status', 1)->orderBy('sort')->get();
         return $response;
