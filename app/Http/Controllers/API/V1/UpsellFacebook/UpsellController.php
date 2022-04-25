8782b4def8e39d68b9847b1ad7e14a61c964c3e5
Mahir-BS23-21J7 <mahir.shahrier@brainstation-23.com>
2022-04-25 10:33:47 +0600

    Progress: Refactored

--- app/Http/Controllers/API/V1/UpsellFacebook/UpsellController.php
+++ app/Http/Controllers/API/V1/UpsellFacebook/UpsellController.php
@@ -15,16 +15,25 @@ use Illuminate\Http\Request;
 class UpsellController extends Controller
 {
     private $customerService, $productService, $balanceService;
-    public function __construct(CustomerService $customerService, ProductService $productService, BalanceService $balanceService){
-
+    
+    public function __construct(
+        CustomerService $customerService, 
+        ProductService $productService, 
+        BalanceService $balanceService
+    ) {
         $this->customerService = $customerService;
         $this->productService = $productService;
         $this->balanceService = $balanceService;
     }
+
+    /**
+     * POST Request BODY
+     * msisdn: string,
+     * product_code: string
+     * pay_with_balance: boolean|string
+     */
     public function phaseOne(Request $request)
     {
-        // OTP
-
         /**
          * OTP JOURNEY
          * 
@@ -35,33 +44,8 @@ class UpsellController extends Controller
          * 5. 4 > 3 Return OTP page & Send OTP to Customer Phone
          * 6. 4 < 3 Return Primary Error
          */
-        $customer = $this->customerService->getCustomerInfoByPhone($request->msisdn);
-        $product = $this->productService->getProductByCode( $request->product_code);
-        $result = $this->productService->eligible($request->msisdn, $request->product_code);
-        $customerStatus =  $result->getData();
-        $customerType = $customer->number_type;
-        if($customerStatus->status_code!=200){
-            dd("error");
-        }
 
-        $productPrice = $product->productCore->price;
-        if($customerType == 'prepaid'){
-            $customerBalance = $this->balanceService->getPrepaidBalance($customer->id);
-
-            if ($productPrice > $customerBalance) {
-                dd("You don't have enough balance to purchase this package.");
-            }
-        }
-        
-        if($customerType == 'postpaid'){
-            $customerBalance = $this->balanceService->getPostpaidBalance($customer->id);
-
-            if ($productPrice > $customerBalance) {
-                dd("You don't have enough balance to purchase this package.");
-            }
-        }
-        
-         /**
+        /**
          * CURRENCY JOURNEY
          * 
          * 1. Find Customer By Phone No
@@ -69,9 +53,43 @@ class UpsellController extends Controller
          * 3. get product cost
          * 4. Return Payment Page
          */
-         
-        
 
+        $customer = $this->customerService->getCustomerInfoByPhone($request->msisdn);
+        $product = $this->productService->getProductByCode( $request->product_code);
+        $result = $this->productService->eligible($request->msisdn, $request->product_code);
+        $customerStatus =  $result->getData();
+        $customerType = $customer->number_type;
+
+        if($customerStatus->status_code != 200){
+            dd("error");
+        }
+
+        $productPrice = $product->productCore->price;
+
+        if($request->pay_with_balance) {
+            if($customerType == 'prepaid'){
+                $customerBalance = $this->balanceService->getPrepaidBalance($customer->id);
+    
+                if ($productPrice > $customerBalance) {
+                    dd("You don't have enough balance to purchase this package.");
+                }
+            }
+            
+            if($customerType == 'postpaid'){
+                $customerBalance = $this->balanceService->getPostpaidBalance($customer->id);
+    
+                if ($productPrice > $customerBalance) {
+                    dd("You don't have enough balance to purchase this package.");
+                }
+            }
+
+            // SEND OTP TO MSISDN
+
+            return 'OTP VERIFICATION PAGE VIEW with [customer no, product_code & detail, Cost]';
+        }
+
+        return 'BANK / MOBILE PAYMENT VIEW with [customer no, product_code & detail, Cost]';
+        
     }
 
     public function phaseTwo()
