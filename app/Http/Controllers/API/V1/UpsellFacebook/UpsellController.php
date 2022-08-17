72f910775d73765d18802aeccdefa196648f14ee
Mahir-BS23-21J7 <mahir.shahrier@brainstation-23.com>
2022-08-17 10:38:57 +0600

    Update added hash in signature

--- app/Http/Controllers/API/V1/UpsellFacebook/UpsellController.php
+++ app/Http/Controllers/API/V1/UpsellFacebook/UpsellController.php
@@ -113,9 +113,12 @@ class UpsellController extends Controller
         $productValidity = $productDetails['details']['validity'];
         $productDisplayTitleEn = $productDetails['details']['display_title_en']; 
         // str_replace(' ', '%20', $productDetails['details']['display_title_en']);
+        
+        $secret = env("UPSELL_SECRET");
         $timestamp = Carbon::now()->timestamp;
         $signature = strrev(base64_encode($timestamp * $timestamp));
-
+        $hash = hash_hmac('sha256', $signature, $secret, true);
+        $signature = $signature . $timestamp . $hash;
         
 
         // $customerIsEligibleForProduct = $this->upsellService->customerIsEligibleForProduct($msisdn, $productCode);
