486bc5878d7580d19dd55ffe1e1fe3a62a381c32
Jahidul Islam <jahidul@brainstation-23.com>
2022-11-16 16:09:04 +0600

    Remove base url

--- app/Http/Resources/ManagementResource.php
+++ app/Http/Resources/ManagementResource.php
@@ -19,16 +19,16 @@ class ManagementResource extends JsonResource
             'name_en'                       => $this->name ?? null,
             'name_bn'                       => $this->name_bn ?? null,
             'designation_en'                => $this->designation ?? null,
-            'designation_bn'                =>$this->designation_bn ?? null,
-            'banner_image'                  => env('IMAGE_HOST_URL') . "/" . $this->banner_image ?? null,
-            'profile_image'                 => env('IMAGE_HOST_URL') . "/" . $this->profile_image ?? null,
+            'designation_bn'                => $this->designation_bn ?? null,
+            'banner_image'                  => $this->banner_image ?? null,
+            'profile_image'                 => $this->profile_image ?? null,
             'personal_details_en'           => $this->personal_details ?? null,
             'personal_details_bn'           => $this->personal_details_bn ?? null,
             'twitter_link'                  => $this->twitter_link ?? null,
             'linkedin_link'                 => $this->linkedin_link ?? null,
             'facebook_link'                 => $this->facebook_link ?? null,
             'others_link'                   => $this->others_link ?? null,
-            'display_order'                   => $this->display_order ?? null
+            'display_order'                 => $this->display_order ?? null
 
         ];
 
