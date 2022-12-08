ab2f8d82520f58b0c6b904d43620cd9848870574
Jahidul Islam <jahidul@brainstation-23.com>
2022-12-08 17:20:57 +0600

    get orange club offer by category

---
+++ app/Repositories/PartnerOfferCategoryRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: BS23
+ * Date: 27-Aug-19
+ * Time: 3:51 PM
+ */
+
+namespace App\Repositories;
+
+use App\Models\PartnerCategory;
+
+class PartnerOfferCategoryRepository extends BaseRepository
+{
+    public $modelName = PartnerCategory::class;
+
+    public function loyaltyCatOffers()
+    {
+        return $this->model->where('status', 1)
+            ->whereHas('partnerOffers')
+            ->select(
+                'id',
+                'name_en',
+                'name_bn',
+                'url_slug_en',
+                'url_slug_bn',
+                'page_header',
+                'page_header_bn',
+                'schema_markup'
+            )
+            ->with(['partnerOffers' => function ($q) {
+                $q->where('is_active', 1);
+                $q->select(
+                    'partner_id',
+                    'partner_category_id',
+                    'loyalty_tier_id',
+                    'card_img',
+                    'validity_en',
+                    'validity_bn',
+                    'offer_unit',
+                    'offer_value',
+                    'offer_scale'
+                )
+                ->with([ 'partner' => function ($q){
+                    $q->select('id', 'company_name_en', 'company_name_bn');
+                }]);
+            }])
+            ->get();
+    }
+}
