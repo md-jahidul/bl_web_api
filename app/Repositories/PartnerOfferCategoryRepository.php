3bc38b6b715c9c7b2aeedc6bce01a56a1be74fef
Jahidul Islam <jahidul@brainstation-23.com>
2023-01-02 12:33:52 +0600

    Loyalty offer issue fix

--- app/Repositories/PartnerOfferCategoryRepository.php
+++ app/Repositories/PartnerOfferCategoryRepository.php
@@ -32,15 +32,21 @@ class PartnerOfferCategoryRepository extends BaseRepository
             ->with(['partnerOffers' => function ($q) {
                 $q->where('is_active', 1);
                 $q->select(
+                    'id',
                     'partner_id',
                     'partner_category_id',
                     'loyalty_tier_id',
                     'card_img',
                     'validity_en',
                     'validity_bn',
-                    'offer_unit',
-                    'offer_value',
-                    'offer_scale'
+                    'btn_text_en',
+                    'btn_text_bn',
+                    'url_slug',
+                    'url_slug_bn',
+                    'page_header',
+                    'page_header_bn',
+                    'schema_markup',
+                    'other_attributes'
                 )
                 ->with([ 'partner' => function ($q){
                     $q->select('id', 'company_name_en', 'company_name_bn');
