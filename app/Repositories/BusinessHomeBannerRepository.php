76490feb5b827172931d64bb253e61a726804b0c
abusabir_BL <abu.sabir@annanovas.com>
2023-01-15 16:10:35 +0600

    Image URL issue solve

--- app/Repositories/BusinessHomeBannerRepository.php
+++ app/Repositories/BusinessHomeBannerRepository.php
@@ -18,8 +18,8 @@ class BusinessHomeBannerRepository extends BaseRepository {
         $banners = $this->model->get();
         $count = 0;
         foreach($banners as $v){
-            $data[$count]['image'] = $v->image_name == "" ? "" : config('filesystems.image_host_url'). $v->image_name;
-            $data[$count]['image_mobile'] = $v->image_name_mobile == "" ? "" : config('filesystems.image_host_url'). $v->image_name_mobile;
+            $data[$count]['image'] = $v->image_name == "" ? "" : $v->image_name;
+            $data[$count]['image_mobile'] = $v->image_name_mobile == "" ? "" : $v->image_name_mobile;
             $data[$count]['alt_text'] = $v->alt_text;
             $data[$count]['sort'] = $v->home_sort == 1 ? 'left' : 'right';
             $count++;
