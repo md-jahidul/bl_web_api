e5f69c2270a57b1d7e2a7bb03bb9f7f2cead08a1
shuvo-bs-23 <mehedi.shuvo@brainstation-23.com>
2023-01-24 02:08:39 +0600

    al-banner for dynamic page

---
+++ app/Repositories/AlBannerRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\AlBanner;
+
+/**
+ * Class AlBannerRepository
+ * @package App\Repositories
+ */
+class AlBannerRepository extends BaseRepository
+{
+    public $modelName = AlBanner::class;
+
+}
