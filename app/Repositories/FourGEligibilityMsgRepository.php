d3064d7ca5588588200cad2c86a24300951a401d
fahreyad <fahreyad.hossain@brainstation-23.com>
2023-01-31 18:28:28 +0600

    added

---
+++ app/Repositories/FourGEligibilityMsgRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+namespace App\Repositories;
+
+use App\Models\FourGEligibilityMsg;
+
+class FourGEligibilityMsgRepository extends BaseRepository
+{
+    public $modelName = FourGEligibilityMsg::class;
+
+}
