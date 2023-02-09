7d15676be8ba58ba3006ead9618aebba4c924c5b
fahreyad <fahreyad.hossain@brainstation-23.com>
2023-02-09 14:28:21 +0600

    on progress

--- app/Services/EcareerService.php
+++ app/Services/EcareerService.php
@@ -553,6 +553,8 @@ class EcareerService
 
                         $sub_items['title_en'] = $portal_items->title_en;
                         $sub_items['title_bn'] = $portal_items->title_bn;
+                        $sub_items['sub_title_en'] = $portal_items->sub_title_en;
+                        $sub_items['sub_title_bn'] = $portal_items->sub_title_bn;
                         $sub_items['description_en'] = $portal_items->description_en;
                         $sub_items['description_bn'] = $portal_items->description_bn;
                         $sub_items['image'] = !empty($portal_items->image) ?  $portal_items->image : null;
@@ -1139,6 +1141,11 @@ class EcareerService
         if(isset($programId)){
             $programList = $this->ecarrerPortalItemRepository->findProgramList($programId->id,$type);
             $programs = [];
+            $programs['title_en'] = $programId->title_en;
+            $programs['title_bn'] = $programId->title_bn;
+            $programs['description_en'] = $programId->description_en;
+            $programs['description_bn'] = $programId->description_bn;
+            //$programs = $programId->title_bn;
             if(!empty($programList)){
 
                 foreach ($programList as $program){
@@ -1154,7 +1161,7 @@ class EcareerService
                     $response['alt_links'] = $program->alt_links;
                     $response['video'] = $program->video;
                     $response['call_to_action_buttons'] = !empty($program->call_to_action) ? unserialize($program->call_to_action) : null;
-                    $programs[] =  $response;
+                    $programs['item'] =  $response;
                 }
             }
             return $programs;
