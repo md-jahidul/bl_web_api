0fab92e811a0fb5299e5b4e204bb66f7ccb283c5
BS956 <jahidul@brainstation-23.com>
2023-02-24 16:18:44 +0600

    Offer details issue fix

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -579,8 +579,12 @@ class ProductService extends ApiBaseService
                 unset($productDetail->related_product);
                 unset($productDetail->productCore);
 
-                $banner = $this->alBannerService->getBanner($productDetail->id, 'product_details');
-
+                if (isset($productDetail->offer_category->alias) && $productDetail->offer_category->alias == "new_sim_offer") {
+                    $banner = $this->alBannerService->getBanner($productDetail->id, 'product_other_details');
+                } else {
+                    $banner = $this->alBannerService->getBanner($productDetail->id, 'product_details');
+                }
+                unset($productDetail->offer_category);
                 $productDetail->product_details->banner_image_url = $banner->image ?? null;
                 $productDetail->product_details->banner_title_en = $banner->title_en ?? null;
                 $productDetail->product_details->banner_title_bn = $banner->title_bn ?? null;
