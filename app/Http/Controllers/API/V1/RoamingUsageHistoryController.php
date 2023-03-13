d5e40cef3d7dbaf2088687722ac2af31e74c821e
BS956 <jahidul@brainstation-23.com>
2023-03-13 18:39:35 +0600

    Add all useges history api implement

--- app/Http/Controllers/API/V1/RoamingUsageHistoryController.php
+++ app/Http/Controllers/API/V1/RoamingUsageHistoryController.php
@@ -12,29 +12,28 @@ class RoamingUsageHistoryController extends Controller
 {
     protected $service;
 
-    public function __construct(CustomerRoamingUsageService $service)
+    public function __construct(CustomerRoamingUsageService $roamingUsageService)
     {
-        $this->service = $service;
-       // $this->middleware('idp.verify');
+        $this->roamingUsageService = $roamingUsageService;
     }
 
     public function getSummaryUsageHistory(UsageHistoryRequest $request)
     {
-        return $this->service->getSummaryUsageHistory($request);
+        return $this->roamingUsageService->getSummaryUsageHistory($request);
     }
 
     public function getSmsUsageHistory(UsageHistoryRequest $request)
     {
-        return $this->service->getSmsUsageHistory($request);
+        return $this->roamingUsageService->getSmsUsageHistory($request);
     }
 
     public function getDataUsageHistory(UsageHistoryRequest $request)
     {
-        return $this->service->getDataUsageHistory($request);
+        return $this->roamingUsageService->getDataUsageHistory($request);
     }
 
     public function getCallUsageHistory(UsageHistoryRequest $request)
     {
-        return $this->service->getCallUsageHistory($request);
+        return $this->roamingUsageService->getCallUsageHistory($request);
     }
 }
