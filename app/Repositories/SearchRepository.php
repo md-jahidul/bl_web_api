337c47486dc9b1b57b3601bf4c1ad2dd9a24fb52
BS956 <jahidul@brainstation-23.com>
2023-05-21 17:04:34 +0600

    Modifiy search table modal

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -7,29 +7,24 @@
 
 namespace App\Repositories;
 
+use App\Models\SearchableData;
 use App\Models\SearchData;
 use App\Models\SearchSettings;
 
 class SearchRepository extends BaseRepository {
 
-    public $modelName = SearchData::class;
+    public $modelName = SearchableData::class;
 
     public function searchSuggestion($keyword) {
-        $keywrods = $keyword;
+        $keywords = $keyword;
 
         $kwArray = explode(' ', $keyword);
-
-        $keywrods .= ", " . implode(', ', $kwArray);
-
-        $keywrods .= ", " . $keyword . "*";
-        $keywrods .= ", " . implode('*, ', $kwArray) . "*";
-
-
-
-        $response = $this->model->select('keyword', 'url as product_url', 'type', 'product_code')
-                        ->where('status', 1)
-                        ->whereRaw("MATCH(keyword,tag) AGAINST('$keywrods' IN BOOLEAN MODE)")->get();
-        return $response;
+        $keywords .= ", " . implode(', ', $kwArray);
+        $keywords .= ", " . $keyword . "*";
+        $keywords .= ", " . implode('*, ', $kwArray) . "*";
+        return $this->model->select('product_code', 'page_title_en', 'page_title_bn', 'url_slug_en', 'url_slug_bn')
+                        ->whereRaw("MATCH(page_title_en,page_title_bn,tag_en,tag_bn) AGAINST('$keywords' IN BOOLEAN MODE)")
+                        ->where('status', 1)->get();
     }
 
     public function searchData($keyword) {
