0c9a02bde372b5c1c3653694f00cd7a98ae14512
BS956 <jahidul@brainstation-23.com>
2023-06-07 04:49:21 +0600

    Amar offer issue fix

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -338,7 +338,7 @@ class ProductService extends ApiBaseService
     }
 
     public function prepareAmarOffer(){
-        $amarOffers = $this->amarOfferService->getAmarOfferList(request());
+        $amarOffers = $this->amarOfferService->getAmarOfferListV2(request());
 
         if ($amarOffers->getData()->status_code == 200) {
             $offerCollection = collect($amarOffers->getData()->data)->groupBy('offer_type');
